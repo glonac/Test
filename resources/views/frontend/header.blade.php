5bb77a46399d012f48f81e77f2608c0f6c6c2c1e
Konstantin <glonas22808@mail.ru>
2020-10-25 22:43:18 +0300

    Немного верстки

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -1,8 +1,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
@@ -15,20 +14,24 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>TEST</title>
-</head>
-<nav class="site-header sticky-top py-1">
-    <div class="container d-flex flex-column flex-md-row justify-content-between">
-        <a class="py-2" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-        </a>
-
-        <a class="py-2 d-none d-md-inline-block" href="test">Тест</a>
-        <a class="py-2 d-none d-md-inline-block" href="check">Проверка</a>
-        <a class="py-2 d-none d-md-inline-block" href="trening">Еще кое-что</a>
-        <a class="py-2 d-none d-md-inline-block homehref " id="homehref" href="log">Личный кабинет</a>
-        <a class="py-2 d-none d-md-inline-block enterhref" href="auto">Вход</a>
+    <div class=" align-items-center p-3 px-md-4  border-bottom   site-header"  id="Loyatheader">
+        <nav class="container d-flex flex-column flex-md-row navbar-collapse justify-content-between ">
+            <a  class="p-2 nav-link d-md-inline-block " href="test">Тест</a>
+            <a  class="p-2 nav-link d-md-inline-block "href="check">Проверка</a>
+            <a  class="p-2 nav-link d-md-inline-block homehref" id="homehref" href="log">Личный кабинет</a>
+            <a  class="p-2 nav-link d-md-inline-block enterhref" href="auto">Вход</a>
+        </nav>
     </div>
-</nav>
+{{--        <div class="  p-3 px-md-4  border-bottom  site-header navbar sticky-top py-1">--}}
+{{--            <nav class="container d-flex flex-column flex-md-row navbar-collapse justify-content-between ">--}}
+{{--                <a class="py-2 d-none d-md-inline-block nav-item nav-link active" href="test">Тест</a>--}}
+{{--                <a class="py-2 d-none d-md-inline-block nav-item" href="check">Проверка</a>--}}
+{{--                <a class="py-2 d-none d-md-inline-block nav-item" href="trening">Еще кое-что</a>--}}
+{{--                <a class="py-2 d-none d-md-inline-block homehref nav-item" id="homehref" href="log">Личный кабинет</a>--}}
+{{--                <a class="py-2 d-none d-md-inline-block enterhref nav-item" href="auto">Вход</a>--}}
+{{--            </nav>--}}
+{{--        </div>--}}
+</head>
 <body>
 <div class="container-fluid pl-0 pr-0">
     @yield('log')
@@ -42,7 +45,7 @@
 <footer id="footer">
     <div class="row ml-0 mr-0 pt-4 pb-4">
         <div class="col-12">
-            Герман такой лох уххх!
+            Footer
         </div>
     </div>
 </footer>
--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -1,8 +1,4 @@
 @extends('frontend.header')
 @section('section-main')
-<div class="row" style="margin:0;padding:0;">
-        <div class ="col-xs-12 col-sm-9 col-md-4 col-lg-4 " > X</div>
-        <div class ="col-xs-12 cool-sm-3 col-md-6 col-lg-4" >u</div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4" >Й</div>
-    </div>
+
 @endsection
